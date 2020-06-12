696014ca0da5f3ac768670fb96a1ada70fea7bca
Nomin <knm@iansit.co.kr>
2020-06-12 18:16:36 +0900

    소스코드 모듈화, 샘플데이터 교체

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+using Xbim.COBieLite;
 using Xbim.Ifc;
 using Xbim.ModelGeometry.Scene;
-using Xbim.COBieLite;
 
 namespace IFCtoWexBIM
 {
@@ -14,7 +10,7 @@ namespace IFCtoWexBIM
     {
         static void Main(string[] args)
         {
-            const string filename = "20200404CV.01.ifc";
+            const string filename = "20200404UFCSPA-ARQ-PE-R0-correto.ifc";
 
             using (var model = IfcStore.Open(filename))
             {
@@ -22,32 +18,63 @@ namespace IFCtoWexBIM
                 context.CreateContext();
 
                 var wexBimFilename = Path.ChangeExtension(filename, "wexBim");
-                using (var wexBimfile = File.Create(wexBimFilename))
-                {
-                    using (var wexBimBinaryWriter = new BinaryWriter(wexBimfile))
-                    {
-                        model.SaveAsWexBim(wexBimBinaryWriter);
-                        wexBimBinaryWriter.Close();
-                    }
-                    wexBimfile.Close();
-                }
+                ConvertWexBim(model, wexBimFilename);
 
                 string cobieFileName = Path.ChangeExtension(filename, "json");
-                using (var cobieFile = new FileStream(cobieFileName, FileMode.Create))
-                {
-                    var helper = new CoBieLiteHelper(model, "UniClass");
-                    var fac = helper.GetFacilities().FirstOrDefault();
-                    if (fac != null)
-                    {
-                        using (var writer = new StreamWriter(cobieFile))
-                        {
-                            CoBieLiteHelper.WriteJson(writer, fac);
-                            writer.Close();
-                        }
-                    }
-                }
+                ConvertJson(model, cobieFileName);
                 model.Close();
             }
+        }
+
+        /// <summary>
+        /// IfcStore 모델을 wexbim 파일로 변환하여 저장한다.
+        /// </summary>
+        /// <param name="model">IfcStore 모델</param>
+        /// <param name="filename">저장할 wexbim 파일명</param>
+        /// <returns>성공/실패</returns>
+        static bool ConvertWexBim(IfcStore model, string filename)
+        {
+            bool result = false;
+
+            using (var wexBimfile = File.Create(filename))
+            {
+                using (var wexBimBinaryWriter = new BinaryWriter(wexBimfile))
+                {
+                    model.SaveAsWexBim(wexBimBinaryWriter);
+                    wexBimBinaryWriter.Close();
+                    result = true;
+                }
+                wexBimfile.Close();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// IfcStore 모델을 json 파일로 변환하여 저장한다.
+        /// </summary>
+        /// <param name="model">IfcStore 모델</param>
+        /// <param name="filename">저장할 json 파일명</param>
+        /// <returns></returns>
+        static bool ConvertJson(IfcStore model, string filename)
+        {
+            bool result = false;
+
+            using (var cobieFile = new FileStream(filename, FileMode.Create))
+            {
+                var helper = new CoBieLiteHelper(model, "UniClass");
+                var fac = helper.GetFacilities().FirstOrDefault();
+                if (fac != null)
+                {
+                    using (var writer = new StreamWriter(cobieFile))
+                    {
+                        CoBieLiteHelper.WriteJson(writer, fac);
+                        writer.Close();
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
